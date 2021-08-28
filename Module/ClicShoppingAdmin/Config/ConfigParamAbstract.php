54ecb29f01d8242a3f6ca359d0f5990b98acf016
ClicShopping <admin@clicshopping.org>
2021-08-28 14:43:44 +0200

    ADD TYPAGE

--- Module/ClicShoppingAdmin/Config/ConfigParamAbstract.php
+++ Module/ClicShoppingAdmin/Config/ConfigParamAbstract.php
@@ -14,11 +14,11 @@
   use ClicShopping\OM\Registry;
 
   abstract class ConfigParamAbstract extends \ClicShopping\Sites\ClicShoppingAdmin\ConfigParamAbstract {
-    protected $app;
+    protected mixed $app;
     protected $config_module;
 
-    protected $key_prefix = 'clicshopping_app_new_template_';
-    public $app_configured = true;
+    protected string $key_prefix = 'clicshopping_app_new_template_';
+    public bool $app_configured = true;
 
     public function __construct($config_module) {
         $this->app = Registry::get('New_Template');
